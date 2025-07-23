c067afd865c1e2a272aa947fab065b97a699c644
jzava;a <jzavala@opera.com>
2025-07-23 13:48:23 -0700

    Mac GetCommandLineArgs issue fixed

--- source/EOS_gml/extensions/EpicOnlineServices/epiconlineservices_macos/DesktopExtensionTools.mm
+++ source/EOS_gml/extensions/EpicOnlineServices/epiconlineservices_macos/DesktopExtensionTools.mm
@@ -18,3 +18,25 @@ std::string DesktopExtensionTools_getPathToExe()
     exePath = exePath.parent_path();
     return exePath.string();
 }
+
+std::vector<std::string> GetCommandLineArgs() {
+    std::vector<std::string> args;
+    std::ifstream cmdlineFile("/proc/self/cmdline", std::ios::binary);
+    if (!cmdlineFile)
+        return args;
+
+    // Read the entire file into a string.
+    std::string content((std::istreambuf_iterator<char>(cmdlineFile)),
+        std::istreambuf_iterator<char>());
+
+    // The command-line arguments are null-separated.
+    size_t pos = 0;
+    while (pos < content.size()) {
+        std::string arg(&content[pos]);
+        if (!arg.empty())
+            args.push_back(arg);
+        pos += arg.size() + 1;
+    }
+    return args;
+}
+
